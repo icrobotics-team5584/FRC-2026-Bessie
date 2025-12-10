31834f97aa68e68842bc4bc54cc6539fd82a5b8a
JacobXXI <jacobphhj@gmail.com>
2025-12-10 18:31:44 +1100

    Implement pose estimater

--- src/main/cpp/commands/VisionCommand.cpp
+++ src/main/cpp/commands/VisionCommand.cpp
@@ -1,6 +1,7 @@
 #include "commands/VisionCommand.h"
 #include "subsystems/SubDrivebase.h"
 #include "subsystems/SubVision.h"
+#include "subsystems/PoseEstimater.h"
 
 namespace cmd {
 using namespace frc2::cmd;
@@ -8,8 +9,8 @@ using namespace frc2::cmd;
 frc2::CommandPtr AddVisionMeasurement() {
     auto poses = SubVision::GetInstance().GetEstPose();
     for (auto pose : poses) {
-        SubDrivebase::GetInstance().AddVisionMeasurement(
-                pose.pose, pose.timestamp, pose.dev);
+        PoseEstimater::GetInstance().AddVisionMeasurement(
+                pose.pose, pose.timestamp, {pose.dev, pose.dev, 0.9});
     }
 }
 
---
+++ src/main/cpp/subsystems/PoseEstimater.cpp
@@ -0,0 +1,26 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "subsystems/PoseEstimater.h"
+#include <frc/DriverStation.h>
+
+void PoseEstimater::DrivebaseUpdate(frc::Rotation2d gyroAngle, wpi::array<frc::SwerveModulePosition, 4U> &wheelPositions) {
+    auto alliance = frc::DriverStation::GetAlliance();
+    if (alliance.value_or(frc::DriverStation::Alliance::kBlue) ==
+        frc::DriverStation::Alliance::kBlue) {
+        _poseEstimator.Update(gyroAngle, wheelPositions);
+    } else {
+        _poseEstimator.Update(gyroAngle - 180_deg, wheelPositions);
+    }
+
+    _fieldDisplay.SetRobotPose(_poseEstimator.GetEstimatedPosition());
+}
+
+void PoseEstimater::AddVisionMeasurement(frc::Pose2d pose, units::second_t timeStamp, wpi::array<double,3> dev) {
+    _poseEstimator.AddVisionMeasurement(pose, timeStamp, dev);
+}
+
+frc::Pose2d PoseEstimater::GetEstPose() {
+    return _poseEstimator.GetEstimatedPosition();
+}
