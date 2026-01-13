7307abc9ff5b83715155f3ee61c746e875f8b596
furmena <diaschithira21@gmail.com>
2026-01-13 18:51:14 +1100

    added pathplanner+added auton helper

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -25,7 +25,7 @@ void Robot::AutonomousInit() {
   m_autonomousCommand = m_container.GetAutonomousCommand();
 
   if (m_autonomousCommand) {
-    frc2::CommandScheduler::GetInstance().Schedule(m_autonomousCommand.value());
+    m_autonomousCommand->Schedule();
   }
 }
 
