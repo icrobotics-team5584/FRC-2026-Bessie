d4faf81c6668dd0445a60db9774e6a970b7b8e5a
Noah320382 <noahrjenkinson@gmail.com>
2026-01-14 09:47:44 +1100

    Implemented configurations for intake motor as well as intake on and intake off functions

---
+++ src/main/cpp/subsystems/SubIntake.cpp
@@ -0,0 +1,22 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "subsystems/SubIntake.h"
+#include <units/current.h>
+
+SubIntake::SubIntake() {
+   _intakeMotorConfig.SmartCurrentLimit(60); 
+   _intakeMotor.AdjustConfig(_intakeMotorConfig);
+}
+
+ frc2::CommandPtr SubIntake::IntakeOn () {
+    return StartEnd ([this]{_intakeMotor.Set(1);}, [this]{_intakeMotor.Set(0);});
+ };
+
+ frc2::CommandPtr SubIntake::IntakeOff () {
+    return RunOnce ([this]{_intakeMotor.Set(0);});   
+ };
+
+// This method will be called once per scheduler run
+void SubIntake::Periodic() {}
