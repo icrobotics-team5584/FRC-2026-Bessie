ad4cb800ffde4fa8fd7ae905ec8200622babebf9
Liam Stow <liam.stow@outlook.com>
2026-01-13 18:36:32 +1100

    remove unused flex, temp remove configAccessor calls, pigeon to id 13, drivebase pids need retuning

--- src/main/cpp/utilities/ICSpark.cpp
+++ src/main/cpp/utilities/ICSpark.cpp
@@ -60,7 +60,7 @@ rev::REVLibError ICSpark::Configure(rev::spark::SparkBaseConfig &config,
       _configErrorAlert.Set(true);
     }
   }
-  RefreshConfigCache();
+  // RefreshConfigCache(); // TODO: ConfigAccessor seems to be causing segfault, fix.
   return error;
 }
 
