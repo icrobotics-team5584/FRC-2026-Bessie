099e3ee011531847a7e77f7de798aa848a4f04d3
JacobXXI <jacobphhj@gmail.com>
2026-01-15 13:14:36 +1100

    ResetPose function

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -11,6 +11,8 @@
 #include "commands/AutonCommands.h"
 #include "Subsystems/SubVision.h"
 
+#include "utilities/PoseHandler.h"
+
 RobotContainer::RobotContainer() {
   SubDrivebase::GetInstance().SetDefaultCommand(cmd::TeleopDrive(_driverController));
   ConfigureBindings();
@@ -28,6 +30,9 @@ void RobotContainer::ConfigureBindings() {
   _driverController.X().WhileTrue(SubDrivebase::GetInstance().CharacteriseWheels());
   _driverController.Y().OnTrue(SubDrivebase::GetInstance().ResetGyroCmd());
   _driverController.B().OnTrue(SubDrivebase::GetInstance().SyncSensor());
+  _driverController.A().OnTrue(frc2::cmd::RunOnce([]{
+    SubDrivebase::GetInstance().SetPose(frc::Pose2d{0_m,0_m,0_deg});
+  }));
 }
 
 std::shared_ptr<frc2::CommandPtr> RobotContainer::GetAutonomousCommand() {
--- src/main/cpp/subsystems/SubDrivebase.cpp
+++ src/main/cpp/subsystems/SubDrivebase.cpp
@@ -81,15 +81,19 @@ void SubDrivebase::LogDrivebaseStates() {
   _frontRight.SendSensorsToDash();
   _backLeft.SendSensorsToDash();
   _backRight.SendSensorsToDash();
-} 
+}
 
-void SubDrivebase::UpdateOdometry() {
-  wpi::array<frc::SwerveModulePosition, 4U> states = {
+wpi::array<frc::SwerveModulePosition, 4U> SubDrivebase::GetSwerveStates() {
+  return {
     _frontLeft.GetPosition(),
     _frontRight.GetPosition(),
     _backLeft.GetPosition(),
     _backRight.GetPosition()
   };
+}
+
+void SubDrivebase::UpdateOdometry() {
+  wpi::array<frc::SwerveModulePosition, 4U> states = GetSwerveStates();
 
   if (frc::DriverStation::GetAlliance().value_or(frc::DriverStation::Alliance::kBlue) ==
       frc::DriverStation::Alliance::kBlue) {
@@ -395,4 +399,17 @@ frc2::CommandPtr SubDrivebase::CharacteriseWheels() {
     Logger::Log("Drivebase/WheelCharacterisation/BLdelta", BLdelta);
     Logger::Log("Drivebase/WheelCharacterisation/BRdelta", BRdelta);
   })));
+}
+
+void SubDrivebase::SetPose(frc::Pose2d pose) {
+  auto states = GetSwerveStates();
+
+  auto alliance = frc::DriverStation::GetAlliance();
+  if (alliance.value_or(frc::DriverStation::Alliance::kBlue) == frc::DriverStation::Alliance::kBlue) {
+    ResetGyroHeading(pose.Rotation().Degrees());
+  } else {
+    ResetGyroHeading(pose.Rotation().Degrees() - 180_deg);
+  }
+
+  PoseHandler::GetInstance().SetPose(pose, states);
 }
