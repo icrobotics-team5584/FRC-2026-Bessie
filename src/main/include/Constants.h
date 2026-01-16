2ba64e84f862d6504bf91211ba00561d14f120e5
ICWorkshop <icroboticsfll@gmail.com>
2026-01-17 10:54:59 +1100

    changed canid constants for hood, shooter and turret motors

--- src/main/include/Constants.h
+++ src/main/include/Constants.h
@@ -17,10 +17,10 @@ namespace canid {
     constexpr int DRIVEBASE_BACK_LEFT_TURN = 11;
     constexpr int DRIVEBASE_BACK_LEFT_ENCODER = 12; 
 
-    constexpr int HOOD_MOTOR = 14; 
-    constexpr int SHOOTER_MOTOR_1 = 15; 
-    constexpr int SHOOTER_MOTOR_2 = 16;  
-    constexpr int TURRET_MOTOR = 17; 
+    constexpr int HOOD_MOTOR = 19; 
+    constexpr int SHOOTER_MOTOR_1 = 20; 
+    constexpr int SHOOTER_MOTOR_2 = 21;  
+    constexpr int TURRET_MOTOR = 22; 
 
     constexpr int PIGEON_2 = 13; 
     constexpr int INTAKE = 14;
