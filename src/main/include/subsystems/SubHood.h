78a99ddfb76aecc8259031ecdca392e2090b3505
ICWorkshop <icroboticsfll@gmail.com>
2026-01-17 10:50:37 +1100

    also static constexpr gear ratio and arm length for hood

--- src/main/include/subsystems/SubHood.h
+++ src/main/include/subsystems/SubHood.h
@@ -58,12 +58,12 @@ class SubHood : public frc2::SubsystemBase {
   static constexpr bool SIMULATE_GRAVITY = true;
   static constexpr units::degree_t STARTING_ANGLE = 13_deg;
   static constexpr units::degree_t STOW_ANGLE = 12.5_deg;
+  static constexpr double GEAR_RATIO = (42.0/8.0) * (400.0/24.0);
+  static constexpr units::centimeter_t ARM_LENGTH = 20_cm;
 
   bool _resetting = false;
   bool _hasreset = false;
 
-  double GEAR_RATIO = (42.0/8.0) * (400.0/24.0);
-  units::centimeter_t ARM_LENGTH = 20_cm;
   ICSparkMax _hoodMotor{canid::HOOD_MOTOR};
   rev::spark::SparkBaseConfig _hoodMotorConfig;
 
