b93172470daabbf7261e9d1a83f87bc0a914b93d
s-haveesh <shaveesh@gmail.com>
2026-01-18 17:06:01 +1100

    Tuning update (16-01)

--- src/main/include/DrivebaseConfig.h
+++ src/main/include/DrivebaseConfig.h
@@ -12,7 +12,7 @@ namespace DrivebaseConfig {
   constexpr units::meters_per_second_t MAX_DRIVE_TO_POSE_VELOCITY = 2_mps;
   constexpr units::turns_per_second_t MAX_ANGULAR_VELOCITY = 300_deg_per_s;
 
-  static constexpr units::meters_per_second_squared_t MAX_P2P_ACCEL = 3_mps_sq;
+  static constexpr units::meters_per_second_squared_t MAX_P2P_ACCEL = 5_mps_sq;
   static constexpr units::turns_per_second_squared_t MAX_P2P_ANGULAR_ACCEL = 3_tr_per_s_sq;
 
   static constexpr units::turns_per_second_squared_t MAX_ANGULAR_ACCEL{std::numbers::pi};
@@ -35,7 +35,7 @@ namespace DrivebaseConfig {
   const units::turn_t BACK_LEFT_MAG_OFFSET = -0.824951171875_tr;
 
   const frc::ProfiledPIDController<units::meters> TELE_TRANSLATION_PID{2.5, 0, 0, {MAX_VELOCITY, MAX_ACCEL}};
-  const frc::ProfiledPIDController<units::radian> TELE_ROTATION_PID{2.0, 0, 0, {MAX_ANGULAR_VELOCITY, MAX_ANGULAR_ACCEL}};
+  const frc::ProfiledPIDController<units::radian> TELE_ROTATION_PID{3.0, 0, 0.5, {MAX_ANGULAR_VELOCITY, MAX_ANGULAR_ACCEL}};
 
   const pathplanner::PIDConstants AUTO_TRANSLATION_PID{3.2, 0.0, 0.3};
   const pathplanner::PIDConstants AUTO_ROTATION_PID{1.5, 0.0, 0.0};
