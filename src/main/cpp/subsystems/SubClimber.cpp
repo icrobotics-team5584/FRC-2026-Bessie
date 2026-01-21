d813ea09cf65f3c9ff8ff9deb1f697d5aac77700
s-haveesh <shaveesh@gmail.com>
2026-01-21 19:51:31 +1100

    fix indents

--- src/main/cpp/subsystems/SubClimber.cpp
+++ src/main/cpp/subsystems/SubClimber.cpp
@@ -40,71 +40,63 @@ void SubClimber::SetBrakeMode(bool isbrake) {
   if (isbrake) {
     mode.SetIdleMode(rev::spark::SparkBaseConfig::IdleMode::kBrake);
   } else {
-    mode.SetIdleMode(rev::spark::SparkBaseConfig::IdleMode::kBrake);
+    mode.SetIdleMode(rev::spark::SparkBaseConfig::IdleMode::kCoast);
   }
+
   _leftClimbMotor.AdjustConfigNoPersist(mode);
   _rightClimbMotor.AdjustConfigNoPersist(mode);
 }
 
 bool SubClimber::IsAtTarget() {
-  bool rightattarget = false, leftattarget = false;
-  units::turn_t leftrot = _leftClimbMotor.GetPosition();
-  units::turn_t rightrot = _leftClimbMotor.GetPosition();
-  units::turn_t lefttarget = _leftClimbMotor.GetPositionTarget();
-  units::turn_t righttarget = _leftClimbMotor.GetPositionTarget();
+  bool rightAtTarget = false, leftAtTarget = false;
+  units::turn_t leftMotorPosition = _leftClimbMotor.GetPosition();
+  units::turn_t leftMotorTarget = _leftClimbMotor.GetPositionTarget();
+  
+  units::turn_t rightMotorPosition = _rightClimbMotor.GetPosition();
+  units::turn_t rightMotorTarget = _rightClimbMotor.GetPositionTarget();
 
-  if (rightrot < righttarget + TOLERANCE && rightrot > righttarget - TOLERANCE) {
-    rightattarget = true;
+  if (rightMotorPosition < (rightMotorTarget + TOLERANCE) && rightMotorPosition > (rightMotorTarget - TOLERANCE)) {
+    rightAtTarget = true;
   }
 
-  if (leftrot < lefttarget + TOLERANCE && leftrot > lefttarget - TOLERANCE) {
-    leftattarget = true;
+  if (leftMotorPosition < (leftMotorTarget + TOLERANCE) && leftMotorPosition > (leftMotorTarget - TOLERANCE)) {
+    leftAtTarget = true;
   }
 
-  return rightattarget && leftattarget;
+  return (rightAtTarget && leftAtTarget);
 }
 
-units::ampere_t  SubClimber::GetLeftMotorCurrent()
-{
-    return (units::ampere_t)_leftClimbMotor.GetOutputCurrent();
+units::ampere_t SubClimber::GetLeftMotorCurrent() {
+  return (units::ampere_t)_leftClimbMotor.GetOutputCurrent();
 }
 
-units::ampere_t  SubClimber::GetRightMotorCurrent()
-{
-    return (units::ampere_t)_rightClimbMotor.GetOutputCurrent();
+units::ampere_t SubClimber::GetRightMotorCurrent() {
+  return (units::ampere_t)_rightClimbMotor.GetOutputCurrent();
 }
-
 
 /* Commands */
-frc2::CommandPtr SubClimber::WaitUntilReset()
-{
-    return frc2::cmd::RunOnce([this]{ _hasReset = false; })
-        .AndThen(frc2::cmd::Run([this]
-        {
-            if(GetLeftMotorCurrent() > ZEROING_CURRENT && GetRightMotorCurrent() > ZEROING_CURRENT) {
-                _hasReset = true;
-            }
-            if (frc::RobotBase::IsSimulation() == true) {
-                _hasReset = true;
-            }
-        }))
-        .Until([this]{ return _hasReset; });
+frc2::CommandPtr SubClimber::WaitUntilReset() {
+  return frc2::cmd::RunOnce([this]{ _hasReset = false; }).AndThen(frc2::cmd::Run([this] {
+    if(GetLeftMotorCurrent() > ZEROING_CURRENT && GetRightMotorCurrent() > ZEROING_CURRENT) {
+      _hasReset = true;
+    }
+    if (frc::RobotBase::IsSimulation() == true) {
+      _hasReset = true;
+    }
+  })).Until([this]{ return _hasReset; });
 }
 
-frc2::CommandPtr SubClimber::ReadyClimber() 
-{
+frc2::CommandPtr SubClimber::ReadyClimber() {
   _leftClimbMotor.SetPositionTarget(LEFT_READY_TURNS);
   _rightClimbMotor.SetPositionTarget(RIGHT_READY_TURNS);
 }
 
-frc2::CommandPtr SubClimber::ClimbL1() 
-{
+frc2::CommandPtr SubClimber::ClimbL1() {
   _leftClimbMotor.SetPositionTarget(LEFT_L1_TURNS);
   _rightClimbMotor.SetPositionTarget(RIGHT_L1_TURNS);
 }
 
-// frc2::CommandPtr SubClimber::ClimbL2() 
-// {
+// frc2::CommandPtr SubClimber::ClimbL2() {
 //   _leftClimbMotor.SetPositionTarget(LEFT_L2_TURNS);
 //   _rightClimbMotor.SetPositionTarget(RIGHT_L2_TURNS);
 // }
