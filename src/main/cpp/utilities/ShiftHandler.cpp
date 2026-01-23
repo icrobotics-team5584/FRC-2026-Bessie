225b2d5362a99e41aa3eacaa2c9b12ba713acdbe
furmena <diaschithira21@gmail.com>
2026-01-24 10:56:49 +1100

    auto format document

--- src/main/cpp/utilities/ShiftHandler.cpp
+++ src/main/cpp/utilities/ShiftHandler.cpp
@@ -1,130 +1,123 @@
 #include "utilities/ShiftHandler.h"
+
 #include "utilities/Logger.h"
 
+RebuiltShift ShiftHandler::GetCurrentShift() {
+  if (frc::DriverStation::IsAutonomousEnabled()) {
+    return RebuiltShift::AUTON;
+  }
+  units::second_t secondsPassed = frc::DriverStation::GetMatchTime();
+  if (secondsPassed == -1_s) {
+    return RebuiltShift::NONE;
+  }
 
-RebuiltShift ShiftHandler::GetCurrentShift()
-{
-    if(frc::DriverStation::IsAutonomousEnabled()) {
-        return RebuiltShift::AUTON;
-    }
-    units::second_t secondsPassed = frc::DriverStation::GetMatchTime();
-    if(secondsPassed == -1_s) {
-        return RebuiltShift::NONE;
-    }
+  Logger::Log("ShiftHandler/matchTime", secondsPassed);
+  Logger::Log("ShiftHandler/realMatchTime", frc::DriverStation::GetMatchTime());
 
-    Logger::Log("ShiftHandler/matchTime", secondsPassed);
-    Logger::Log("ShiftHandler/realMatchTime", frc::DriverStation::GetMatchTime());
+  if (secondsPassed > 130_s) {
+    return RebuiltShift::TRANS;
+  }
 
-    if(secondsPassed > 130_s) {
-        return RebuiltShift::TRANS;
-    }
-
-    RebuiltShift losingShift, winningShift = GetWinningShift();
-    if(winningShift == RebuiltShift::NONE) {
-        losingShift = RebuiltShift::NONE;
-    } else { /* filps RebuiltShift::BLUE to RebuiltShift::RED and vice versa*/
-        losingShift = (winningShift == RebuiltShift::BLUE ? RebuiltShift::RED : RebuiltShift::BLUE);
-    }
-    if(secondsPassed > 105_s) { /* Shift 1 */
-        return losingShift;
-    }
-    if(secondsPassed > 80_s) { /* Shift 2 */
-        return winningShift;
-    }
-    if(secondsPassed > 55_s) { /* Shift 3 */
-        return losingShift;
-    }
-    if(secondsPassed > 30_s) { /* Shift 4 */
-        return winningShift;
-    }
-    return RebuiltShift::ENDGAME;
+  RebuiltShift losingShift, winningShift = GetWinningShift();
+  if (winningShift == RebuiltShift::NONE) {
+    losingShift = RebuiltShift::NONE;
+  } else { /* filps RebuiltShift::BLUE to RebuiltShift::RED and vice versa*/
+    losingShift = (winningShift == RebuiltShift::BLUE ? RebuiltShift::RED : RebuiltShift::BLUE);
+  }
+  if (secondsPassed > 105_s) { /* Shift 1 */
+    return losingShift;
+  }
+  if (secondsPassed > 80_s) { /* Shift 2 */
+    return winningShift;
+  }
+  if (secondsPassed > 55_s) { /* Shift 3 */
+    return losingShift;
+  }
+  if (secondsPassed > 30_s) { /* Shift 4 */
+    return winningShift;
+  }
+  return RebuiltShift::ENDGAME;
 }
 
-RebuiltShift ShiftHandler::GetWinningShift()
-{
-    std::string data = frc::DriverStation::GetGameSpecificMessage();
-    if(data.length() < 0) { /* No winning shift message recieved */
-        return RebuiltShift::NONE;
-    }
+RebuiltShift ShiftHandler::GetWinningShift() {
+  std::string data = frc::DriverStation::GetGameSpecificMessage();
+  if (data.length() < 0) { /* No winning shift message recieved */
+    return RebuiltShift::NONE;
+  }
 
-    switch(data[0]) {
+  switch (data[0]) {
     case 'B':
-        return RebuiltShift::BLUE;
+      return RebuiltShift::BLUE;
     case 'R':
-        return RebuiltShift::RED;
-    }
+      return RebuiltShift::RED;
+  }
 
-    return RebuiltShift::NONE; /* Corrupt data */
+  return RebuiltShift::NONE; /* Corrupt data */
 }
 
-units::second_t ShiftHandler::GetTimeLeft()
-{
-    units::second_t matchTime = frc::DriverStation::GetMatchTime();
-    if(frc::DriverStation::IsAutonomousEnabled()) {
-        return matchTime;
-    }
-
-    if(matchTime > 130_s) {
-        return matchTime - 130_s;
-    }
-    if(matchTime > 105_s) { /* Shift 1 */
-        return matchTime - 105_s;
-    }
-    if(matchTime > 80_s) { /* Shift 2 */
-        return matchTime - 80_s;
-    }
-    if(matchTime > 55_s) { /* Shift 3 */
-        return matchTime - 55_s;
-    }
-    if(matchTime > 30_s) { /* Shift 4 */
-        return matchTime - 30_s;
-    }
+units::second_t ShiftHandler::GetTimeLeft() {
+  units::second_t matchTime = frc::DriverStation::GetMatchTime();
+  if (frc::DriverStation::IsAutonomousEnabled()) {
     return matchTime;
+  }
+
+  if (matchTime > 130_s) {
+    return matchTime - 130_s;
+  }
+  if (matchTime > 105_s) { /* Shift 1 */
+    return matchTime - 105_s;
+  }
+  if (matchTime > 80_s) { /* Shift 2 */
+    return matchTime - 80_s;
+  }
+  if (matchTime > 55_s) { /* Shift 3 */
+    return matchTime - 55_s;
+  }
+  if (matchTime > 30_s) { /* Shift 4 */
+    return matchTime - 30_s;
+  }
+  return matchTime;
 }
 
-std::string ShiftHandler::GetShiftName(RebuiltShift shift)
-{
-    switch(shift) {
+std::string ShiftHandler::GetShiftName(RebuiltShift shift) {
+  switch (shift) {
     case RebuiltShift::AUTON:
-        return "Autonomous";
+      return "Autonomous";
     case RebuiltShift::TRANS:
-        return "Transition";
+      return "Transition";
     case RebuiltShift::BLUE:
-        return "Blue";
+      return "Blue";
     case RebuiltShift::RED:
-        return "Red";
+      return "Red";
     case RebuiltShift::ENDGAME:
-        return "Endgame";
-    }
+      return "Endgame";
+  }
 
-    return "None";
+  return "None";
 }
 
-bool ShiftHandler::IsShift(RebuiltShift shift)
-{
-    return GetCurrentShift() == shift ? true : false; /* check if matching */
+bool ShiftHandler::IsShift(RebuiltShift shift) {
+  return GetCurrentShift() == shift ? true : false; /* check if matching */
 }
 
-bool ShiftHandler::IsActiveShift()
-{
-    units::second_t matchTime = frc::DriverStation::GetMatchTime();
-    RebuiltShift currentShift = GetCurrentShift();
-    frc::DriverStation::Alliance allicance = frc::DriverStation::GetAlliance().value_or(frc::DriverStation::Alliance::kBlue);
-    bool wonAutonShift = (allicance == frc::DriverStation::Alliance::kBlue) && (currentShift == RebuiltShift::BLUE);
-    bool shift2 = 105_s > matchTime && matchTime > 80_s;     /* 1:45 - 80s */
-    bool shift4 = 55_s > matchTime && matchTime > 30_s;    /* 0:55 - 0:30 */
+bool ShiftHandler::IsActiveShift() {
+  units::second_t matchTime = frc::DriverStation::GetMatchTime();
+  RebuiltShift currentShift = GetCurrentShift();
+  frc::DriverStation::Alliance allicance =
+    frc::DriverStation::GetAlliance().value_or(frc::DriverStation::Alliance::kBlue);
+  bool wonAutonShift =
+    (allicance == frc::DriverStation::Alliance::kBlue) && (currentShift == RebuiltShift::BLUE);
+  bool shift2 = 105_s > matchTime && matchTime > 80_s; /* 1:45 - 80s */
+  bool shift4 = 55_s > matchTime && matchTime > 30_s;  /* 0:55 - 0:30 */
 
-    if(
-        currentShift == RebuiltShift::AUTON ||
-        currentShift == RebuiltShift::TRANS ||
-        currentShift == RebuiltShift::ENDGAME
-    ) {
-        return true;
-    }
+  if (currentShift == RebuiltShift::AUTON || currentShift == RebuiltShift::TRANS ||
+      currentShift == RebuiltShift::ENDGAME) {
+    return true;
+  }
 
-    if(wonAutonShift && (shift2||shift4)) {
-        return true;
-    }
+  if (wonAutonShift && (shift2 || shift4)) {
+    return true;
+  }
 
-    return false;
+  return false;
 }
--- src/main/include/utilities/ShiftHandler.h
+++ src/main/include/utilities/ShiftHandler.h
@@ -1,23 +1,25 @@
-#include <units/time.h>
 #include <frc/DriverStation.h>
 
+#include <units/time.h>
+
 enum RebuiltShift {
-    NONE,
-    AUTON,
-    TRANS,
-    RED,
-    BLUE,
-    ENDGAME,
+  NONE,
+  AUTON,
+  TRANS,
+  RED,
+  BLUE,
+  ENDGAME,
 };
 
 class ShiftHandler {
-    public:
-        static RebuiltShift GetCurrentShift();
-        static RebuiltShift GetWinningShift();
-        static units::second_t GetTimeLeft();
-        static std::string GetShiftName(RebuiltShift shift);
-        static bool IsShift(RebuiltShift shift);
-        static bool IsActiveShift();
-    private:
-        static units::second_t _GetMatchTime();
+ public:
+  static RebuiltShift GetCurrentShift();
+  static RebuiltShift GetWinningShift();
+  static units::second_t GetTimeLeft();
+  static std::string GetShiftName(RebuiltShift shift);
+  static bool IsShift(RebuiltShift shift);
+  static bool IsActiveShift();
+
+ private:
+  static units::second_t _GetMatchTime();
 };
