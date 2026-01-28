74a49c2bb4d743e9794fc93b444ac391e7c063a5
s-haveesh <shaveesh@gmail.com>
2026-01-28 19:00:50 +1100

    Prepare for full auton testing

--- src/main/cpp/commands/AutonCommands.cpp
+++ src/main/cpp/commands/AutonCommands.cpp
@@ -1,5 +1,8 @@
 #include "commands/AutonCommands.h"
+#include "commands/FuelCommands.h"
 #include "subsystems/SubDrivebase.h"
+#include "subsystems/SubIntake.h"
+#include "subsystems/SubShooter.h"
 
 namespace cmd {
     frc2::CommandPtr DefaultAuton() {
@@ -26,11 +29,22 @@ namespace cmd {
     
     frc2::CommandPtr NeutralScoreAndClimb_LeftBump() {
         return frc2::cmd::Sequence(
-            //STARTING POSITION: left bump (X 3.58m, Y 5.80m, heading 0 degrees)
-            SubDrivebase::GetInstance().DriveToPose([] { return frc::Pose2d{7.8_m, 7.0_m, -90_deg}; }, 1.0), //entry to neutral zone
-            SubDrivebase::GetInstance().DriveToPose([] { return frc::Pose2d{7.8_m, 4.75_m, -90_deg}; }, 1.0, 20_cm), //exit from neutral zone
+            // STARTING POSITION: left bump (X 3.58m, Y 5.80m, heading 0 degrees)
+
+            SubDrivebase::GetInstance().DriveToPose([] { return frc::Pose2d{6.3_m, 7.0_m, -90_deg}; }, 1.0),
+            //REPLACE ABOVE WITH: SubDrivebase::GetInstance().DriveToPose([] { return frc::Pose2d{7.8_m, 7.0_m, -90_deg}; }, 1.0), //entry to neutral zone (NeutralInLeft)
+
+            SubDrivebase::GetInstance().DriveToPose([] { return frc::Pose2d{6.3_m, 4.75_m, -90_deg}; }, 1.0, 20_cm)
+                .AlongWith(SubIntake::GetInstance().IntakeOn()),
+            //REPLACE ABOVE WITH: SubDrivebase::GetInstance().DriveToPose([] { return frc::Pose2d{7.8_m, 4.75_m, -90_deg}; }, 1.0, 20_cm).AlongWith(SubIntake::GetInstance().IntakeOn()), //intake until exit from neutral zone (NeutralEndLeft)
+            
             SubDrivebase::GetInstance().DriveToPose([] { return frc::Pose2d{3.58_m, 5.2_m, 180_deg}; }, 1.0, 20_cm), //re-entry to alliance zone (after bump)
-            SubDrivebase::GetInstance().DriveToPose([] { return frc::Pose2d{1.65_m, 3.75_m, 0_deg}; }, 1.0) //tower
+            SubDrivebase::GetInstance().DriveToPose([] { return frc::Pose2d{1.65_m, 3.75_m, 0_deg}; }, 1.0)
+                .AlongWith(cmd::StationaryShootAt(frc::Translation2d{4.625_m, 4.03_m})), //shoot until tower (Tower)
+            //REPLACE ABOVE STATIONARYSHOOTAT WITH SHOOT ON THE MOVE WHEN READY
+
+            cmd::StationaryShootAt(frc::Translation2d{4.625_m, 4.03_m}).WithTimeout(4_s)//, //finish shooting
+            //SubClimber::GetInstance().ClimbL1()
         );
     }
 }
