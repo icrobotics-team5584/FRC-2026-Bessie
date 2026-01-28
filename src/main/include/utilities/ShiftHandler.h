1aa63c29bd2cfea6ec8173c7bc5cb0e24ab00e5f
furmena <diaschithira21@gmail.com>
2026-01-28 20:25:04 +1100

    removed unsed getmatchtime wrapper

--- src/main/include/utilities/ShiftHandler.h
+++ src/main/include/utilities/ShiftHandler.h
@@ -19,7 +19,4 @@ class ShiftHandler {
   static std::string GetShiftName(RebuiltShift shift);
   static bool IsShift(RebuiltShift shift);
   static bool IsActiveShift();
-
- private:
-  static units::second_t _GetMatchTime();
 };
