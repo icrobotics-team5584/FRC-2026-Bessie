155d50d3625e17f21b8f2f5684314338cb3a0271
ICWorkshop <icroboticsfll@gmail.com>
2026-01-31 16:37:34 +1100

    updated auto for going over the bump

--- src/main/cpp/commands/AutonCommands.cpp
+++ src/main/cpp/commands/AutonCommands.cpp
@@ -34,13 +34,17 @@ namespace cmd {
             SubHood::GetInstance().ZeroHood(),
             cmd::ShootOnTheMove().WithTimeout(2_s),
 
-            SubDrivebase::GetInstance().DriveToPose([] { return frc::Pose2d{6.3_m, 7.0_m, -90_deg}; }, 1.0, 5_cm, 5_deg),
+            SubDrivebase::GetInstance().DriveToPose([] { return frc::Pose2d{6.3_m, 5.5_m, -135_deg}; }, 1.0, 5_cm, 5_deg),
+            SubDrivebase::GetInstance().DriveToPose([] { return frc::Pose2d{6.3_m, 5.5_m, -90_deg}; }, 1.0, 5_cm, 5_deg),
             //REPLACE ABOVE WITH: SubDrivebase::GetInstance().DriveToPose([] { return frc::Pose2d{7.8_m, 7.0_m, -90_deg}; }, 1.0), //entry to neutral zone (NeutralInLeft)
  
-            SubDrivebase::GetInstance().DriveToPose([] { return frc::Pose2d{6.3_m, 5.5_m, -90_deg}; }, 0.5, 20_cm, 5_deg)
+            SubDrivebase::GetInstance().DriveToPose([] { return frc::Pose2d{6.3_m, 3.5_m, -90_deg}; }, 0.5, 20_cm, 5_deg)
                 .DeadlineFor(SubIntake::GetInstance().IntakeOn()),
             //REPLACE ABOVE WITH: SubDrivebase::GetInstance().DriveToPose([] { return frc::Pose2d{7.8_m, 4.75_m, -90_deg}; }, 1.0, 20_cm).AlongWith(SubIntake::GetInstance().IntakeOn()), //intake until exit from neutral zone (NeutralEndLeft)
-            
+
+            SubDrivebase::GetInstance().DriveToPose([] { return frc::Pose2d{6.3_m, 5.5_m, -90_deg}; }, 0.5, 20_cm, 5_deg)
+                .DeadlineFor(SubIntake::GetInstance().IntakeOn()),
+
             SubDrivebase::GetInstance().DriveToPose([] { return frc::Pose2d{3.58_m, 5.7_m, 0_deg}; }, 1.0, 20_cm, 5_deg), //re-entry to alliance zone (after bump)
             SubDrivebase::GetInstance().DriveToPose([] { return frc::Pose2d{1.65_m, 3.75_m, 0_deg}; }, 1.0)
                 .AlongWith(cmd::ShootOnTheMove().WithTimeout(6_s))//, //shoot until tower (Tower)
