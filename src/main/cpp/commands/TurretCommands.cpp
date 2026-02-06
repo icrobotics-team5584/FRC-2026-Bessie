25ed146705b511d4476e659cd5d43834e1893bbc
ICWorkshop <icroboticsfll@gmail.com>
2026-02-06 19:28:07 +1100

    formatted for loop correctly

--- src/main/cpp/commands/TurretCommands.cpp
+++ src/main/cpp/commands/TurretCommands.cpp
@@ -1,12 +1,12 @@
 #include "commands/TurretCommands.h"
 
-#include "subsystems/SubHood.h"
-#include "subsystems/SubShooter.h"
-#include "subsystems/SubTurret.h"
-#include "subsystems/SubFeeder.h"
 #include "subsystems/SubDrivebase.h"
+#include "subsystems/SubFeeder.h"
+#include "subsystems/SubHood.h"
 #include "subsystems/SubIndexer.h"
 #include "subsystems/SubIntake.h"
+#include "subsystems/SubShooter.h"
+#include "subsystems/SubTurret.h"
 
 #include "utilities/Logger.h"
 #include "utilities/PoseHandler.h"
@@ -14,24 +14,26 @@
 #include <frc2/command/CommandPtr.h>
 #include <frc2/command/Commands.h>
 
-namespace cmd {  
-   
-  frc2::CommandPtr AimAtFieldRelative(std::function<units::degree_t()> target) {
-    return SubTurret::GetInstance().SetTurretTargetAngle([target] {     
-      auto robotPose = PoseHandler::GetInstance().GetPose();
-      Logger::Log("Turret/AimAtFieldRelative/robotPose/Rotation", robotPose.Rotation().Degrees());
-      units::degree_t targetAngle = target() - robotPose.Rotation().Degrees();
-      return targetAngle;});
-  }
+namespace cmd {
 
-  frc2::CommandPtr AimAtSpot(frc::Translation2d target) {
-    return cmd::AimAtFieldRelative([target] {
-      auto robotPose = PoseHandler::GetInstance().GetPose();
-      units::radian_t angle = atan2( (target.Y()-robotPose.Y()).value(), (target.X()-robotPose.X()).value() ) * 1_rad;
-      units::degree_t degrees = angle;
-      return degrees;
-    });
-  }
+frc2::CommandPtr AimAtFieldRelative(std::function<units::degree_t()> target) {
+  return SubTurret::GetInstance().SetTurretTargetAngle([target] {
+    auto robotPose = PoseHandler::GetInstance().GetPose();
+    Logger::Log("Turret/AimAtFieldRelative/robotPose/Rotation", robotPose.Rotation().Degrees());
+    units::degree_t targetAngle = target() - robotPose.Rotation().Degrees();
+    return targetAngle;
+  });
+}
+
+frc2::CommandPtr AimAtSpot(frc::Translation2d target) {
+  return cmd::AimAtFieldRelative([target] {
+    auto robotPose = PoseHandler::GetInstance().GetPose();
+    units::radian_t angle =
+      atan2((target.Y() - robotPose.Y()).value(), (target.X() - robotPose.X()).value()) * 1_rad;
+    units::degree_t degrees = angle;
+    return degrees;
+  });
+}
 
 units::meter_t CalcShootOnTheMoveDistance() {
   auto target = cmd::TARGETPOSE;
@@ -69,7 +71,7 @@ frc::Pose2d CalcFutureTurretPose() {
   units::meters_per_second_t robotVelX = robotVel.vx;
   units::meters_per_second_t robotVelY = robotVel.vy;
   units::degrees_per_second_t robotVelRot = SubDrivebase::GetInstance().GetAngularVelocity();
-  
+
   Logger::Log("SOTM/velX", robotVelX);
   Logger::Log("SOTM/velY", robotVelY);
   Logger::Log("SOTM/velRot", robotVelRot);
@@ -78,33 +80,32 @@ frc::Pose2d CalcFutureTurretPose() {
   frc::Pose2d futurePose;
 
   for (int i = 0; i < 20; i++) {
+    // Get future pose
+    TOF = SubShooter::GetInstance().GetTimeOfFLightWithDistance(distance);
+    Logger::Log("SOTM/ToF", TOF);
 
-  // Get future pose
-  TOF = SubShooter::GetInstance().GetTimeOfFLightWithDistance(distance);
-  Logger::Log("SOTM/ToF", TOF);
+    // calculate offset due to velocity
+    units::meter_t offsetX = robotVelX * TOF;
+    units::meter_t offsetY = robotVelY * TOF;
+    units::degree_t offsetRot = robotVelRot * TOF;
+    units::degree_t robotRotation = robot.Rotation().Degrees();
+    units::meter_t robotX = robot.X();
+    units::meter_t robotY = robot.Y();
 
-  // calculate offset due to velocity
-  units::meter_t offsetX = robotVelX * TOF;
-  units::meter_t offsetY = robotVelY * TOF;
-  units::degree_t offsetRot = robotVelRot * TOF;
-  units::degree_t robotRotation = robot.Rotation().Degrees();
-  units::meter_t robotX = robot.X();
-  units::meter_t robotY = robot.Y();
+    Logger::Log("SOTM/robotX", robotX);
+    Logger::Log("SOTM/robotY", robotY);
+    Logger::Log("SOTM/robotRotation", robotRotation);
 
-  Logger::Log("SOTM/robotX", robotX);
-  Logger::Log("SOTM/robotY", robotY);
-  Logger::Log("SOTM/robotRotation", robotRotation);
+    Logger::Log("SOTM/offsetX", offsetX);
+    Logger::Log("SOTM/offsetY", offsetY);
+    Logger::Log("SOTM/offsetRot", offsetRot);
 
-  Logger::Log("SOTM/offsetX", offsetX);
-  Logger::Log("SOTM/offsetY", offsetY);
-  Logger::Log("SOTM/offsetRot", offsetRot);
-  
-  // calculate future pose by adding offsets to current robot position
-  futurePose = frc::Pose2d(robotX + offsetX, robotY + offsetY, robot.Rotation().Degrees() + offsetRot);
+    // calculate future pose by adding offsets to current robot position
+    futurePose =
+      frc::Pose2d(robotX + offsetX, robotY + offsetY, robot.Rotation().Degrees() + offsetRot);
 
-  distance = target.Distance(futurePose.Translation());
-
-  } 
+    distance = target.Distance(futurePose.Translation());
+  }
 
   Logger::FieldDisplay::GetInstance().DisplayPose("SOTM/futurePose", futurePose);
 
