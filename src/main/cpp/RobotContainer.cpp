e8e8381c99e741929f818f0cbb6e24e5dca154db
ICWorkshop <icroboticsfll@gmail.com>
2026-02-07 16:44:35 +1100

    commented out the use of cmd::targetpose

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -58,7 +58,7 @@ void RobotContainer::ConfigureBindings() {
   }));
 
   //POVs
-  _driverController.POVUp().OnTrue(cmd::AimAtSpot(cmd::TARGETPOSE));
+  // _driverController.POVUp().OnTrue(cmd::AimAtSpot(cmd::TARGETPOSE));
   _driverController.POVDown().OnTrue(
     SubTurret::GetInstance().SetTurretTargetAngle([] { return 180_deg; }, [] { return 0_deg_per_s; }));
   _driverController.POVRight().OnTrue(cmd::AimAtSpot(frc::Translation2d{0_m, 0_m}));
