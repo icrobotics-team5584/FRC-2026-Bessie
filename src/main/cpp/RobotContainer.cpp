6e4815ad20ae50ae570bce9ee0f369b27ae06266
ICWorkshop <icroboticsfll@gmail.com>
2026-02-13 19:19:36 +1100

    changed binding for driverController to SOTM while driving at constant speed

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -42,7 +42,7 @@ RobotContainer::RobotContainer() {
 void RobotContainer::ConfigureBindings() {
   //Triggers
   _driverController.LeftTrigger().WhileTrue(cmd::IntakeSequence());
-  _driverController.RightTrigger().WhileTrue(cmd::ShootOnTheMove());
+  _driverController.RightTrigger().WhileTrue(cmd::ShootOnTheMove().AlongWith(SubDrivebase::GetInstance().Drive([] {return frc::ChassisSpeeds{0_mps, -1_mps, 0_tps}; }, true)));
   _driverController.RightTrigger().OnFalse(SubFeeder::GetInstance().FeederOff());
 
   //Bumpers
