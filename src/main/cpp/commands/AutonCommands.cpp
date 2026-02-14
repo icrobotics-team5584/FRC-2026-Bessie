ae9c74d83d0f0eed7bc24cf880dd5fc1c8e774ef
s-haveesh <shaveesh@gmail.com>
2026-02-14 11:54:25 +1100

    2a NeutralScoreAndClimb_RightBump

--- src/main/cpp/commands/AutonCommands.cpp
+++ src/main/cpp/commands/AutonCommands.cpp
@@ -94,4 +94,38 @@ namespace cmd {
             //SubClimber::GetInstance().ClimbL1()
         ));
     }
+
+    //DO NOT RUN UNTIL WE HAVE FULL HALF FIELD
+    frc2::CommandPtr NeutralScoreAndClimb_RightBump() {
+        return frc2::cmd::Sequence(
+            // STARTING POSITION: START_BUMP_RIGHT (X 3.58m, Y 2.27m, heading 0 degrees)
+            SubHood::GetInstance().ZeroHood(),
+            cmd::ShootOnTheMove().WithTimeout(2_s)
+        ).AndThen(frc2::cmd::Sequence(
+            SubDrivebase::GetInstance().DriveOverBump(frc::ChassisSpeeds{3_mps, 0_mps, 0_tps}),
+            frc2::cmd::RunOnce([] {
+                SubDrivebase::GetInstance().SetPose(frc::Pose2d{5.7_m, 2.27_m, SubDrivebase::GetInstance().GetGyroAngle()});
+            }), //reset position after traversing the bump
+
+            SubDrivebase::GetInstance().DriveToPose([] { return fieldpos::NEUTRAL_IN_RIGHT; }, 1.0),
+ 
+            SubDrivebase::GetInstance().DriveToPose([] { return fieldpos::NEUTRAL_END_RIGHT; }, 0.5, 20_cm)
+                .DeadlineFor(SubIntake::GetInstance().IntakeOn()),
+
+            SubDrivebase::GetInstance().DriveToPose([] { return frc::Pose2d{5.9_m, 2.9_m, 180_deg}; }, 1.0, 20_cm, 5_deg)
+                .DeadlineFor(SubIntake::GetInstance().IntakeOn()), //return to bump (continue intaking)
+            
+            SubDrivebase::GetInstance().DriveOverBump(frc::ChassisSpeeds{-3_mps, 0_mps, 0_tps}),
+            frc2::cmd::RunOnce([] {
+                SubDrivebase::GetInstance().SetPose(frc::Pose2d{3.5_m, 2.9_m, SubDrivebase::GetInstance().GetGyroAngle()});
+            }) //reset position again
+        ).DeadlineFor(
+            cmd::AimAtSpot(fieldpos::HUB_POSITION.ToTranslation2d())
+        )).AndThen(frc2::cmd::Sequence(
+            SubDrivebase::GetInstance().DriveToPose([] { return fieldpos::TOWER; }, 1.0)
+                .AlongWith(cmd::ShootOnTheMove().WithTimeout(6_s))//, 
+            
+            //SubClimber::GetInstance().ClimbL1()
+        ));
+    }
 }
