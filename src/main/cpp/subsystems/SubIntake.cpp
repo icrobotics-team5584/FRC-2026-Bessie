5a59cc0575837a132af37131d41fc9016720b5a9
Noah320382 <noahrjenkinson@gmail.com>
2026-02-14 15:21:41 +1100

    another fix

--- src/main/cpp/subsystems/SubIntake.cpp
+++ src/main/cpp/subsystems/SubIntake.cpp
@@ -27,8 +27,8 @@ frc2::CommandPtr SubIntake::IntakeOff() {
 
 // This method will be called once per scheduler run
 void SubIntake::Periodic() {
+   auto loopStart = frc::GetTime();
   units::celsius_t intakeTemperature = _intakeMotor.GetTemperature();
-  auto loopStart = frc::GetTime();
 
   units::ampere_t intakeCurrent = _intakeMotor.GetStatorCurrent();
 
