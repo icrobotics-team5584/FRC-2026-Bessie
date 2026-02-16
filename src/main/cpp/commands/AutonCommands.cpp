066858ab6f0d2039e37bcb5640a8d9ea81afc260
Gastatouillle <bouchergaston4@gmail.com>
2026-02-17 00:43:15 +1100

    Create framework for test cases, waiting on dependancies

--- src/main/cpp/commands/AutonCommands.cpp
+++ src/main/cpp/commands/AutonCommands.cpp
@@ -1,7 +1,25 @@
 #include "commands/AutonCommands.h"
+#include "commands/FuelCommands.h"
+#include "subsystems/SubDrivebase.h"
 
 namespace cmd {
     frc2::CommandPtr DefaultAuton() {
         return frc2::cmd::Print("Default Auton");
+    }
+
+
+    //SOTM Test cases WAITING FOR AUTON PR
+    frc2::CommandPtr DriveStraightWhileShooting(){
+        return frc2::cmd::Sequence(
+        
+        ).AlongWith(cmd::ShootOnTheMove());
+    }
+
+
+    frc2::CommandPtr TEMPLATE(){
+        return frc2::cmd::Sequence(
+
+
+        ).AlongWith(cmd::ShootOnTheMove());
     }
 }
--- src/main/include/commands/AutonCommands.h
+++ src/main/include/commands/AutonCommands.h
@@ -2,5 +2,7 @@
 
 namespace cmd {
     frc2::CommandPtr DefaultAuton();
+    frc2::CommandPtr DriveStraightWhileShooting();
+    frc2::CommandPtr TEMPLATE();
 }
 
