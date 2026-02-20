c87672ed83a9ceee82d8533af365e5814ac8c49d
ICWorkshop <icroboticsfll@gmail.com>
2026-02-20 21:07:43 +1100

    uncommented out new auto bindings and tested trench right side horde on robot

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -52,10 +52,10 @@ RobotContainer::RobotContainer() {
 
   _autoManager.AddAuton("Hoard_LeftBump", AutonHelper::MakeCommandPtrAuto(cmd::Hoard_LeftBump()));
   _autoManager.AddAuton("Hoard_LeftTrench", AutonHelper::MakeCommandPtrAuto(cmd::Hoard_LeftTrench()));
-  //_autoManager.AddAuton("Hoard_RightBump",
-  //AutonHelper::MakeCommandPtrAuto(cmd::Hoard_RightBump()));
-  //_autoManager.AddAuton("Hoard_RightTrench",
-  //AutonHelper::MakeCommandPtrAuto(cmd::Hoard_RightTrench()));
+  _autoManager.AddAuton("Hoard_RightBump",
+  AutonHelper::MakeCommandPtrAuto(cmd::Hoard_RightBump()));
+  _autoManager.AddAuton("Hoard_RightTrench",
+  AutonHelper::MakeCommandPtrAuto(cmd::Hoard_RightTrench()));
 
   _autoManager.AddAuton("NeutralAndOutpostScore_RightBump",
     AutonHelper::MakeCommandPtrAuto(cmd::NeutralAndOutpostScore_RightBump()));
