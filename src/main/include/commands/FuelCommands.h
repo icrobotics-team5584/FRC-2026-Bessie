2a0846b595835593e241d0f27f68fca2dfffadd7
ICWorkshop <icroboticsfll@gmail.com>
2026-03-09 14:41:30 +1100

    Added SOTM without turret moving

--- src/main/include/commands/FuelCommands.h
+++ src/main/include/commands/FuelCommands.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <frc2/command/Command.h>
 #include <frc/geometry/Translation2d.h>
+#include <frc2/command/button/CommandXboxController.h>
 
 namespace cmd {
 frc2::CommandPtr IntakeSequence();
@@ -11,7 +12,9 @@ frc2::CommandPtr BackupShoot();
 
 frc2::CommandPtr ShootWhenReady();
 frc2::CommandPtr AimOnTheMove();
+frc2::CommandPtr AimOnTheMoveWithoutRotation(frc2::CommandXboxController& controller);
 frc2::CommandPtr ShootOnTheMove();
+frc2::CommandPtr ShootOnTheMoveWithoutTurret(frc2::CommandXboxController& controller);
 frc2::CommandPtr ToggleBrakeCoast();
 frc2::CommandPtr EjectFuel();
 frc2::CommandPtr DisableAllOverrides();
