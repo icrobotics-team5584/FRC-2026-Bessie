a2d96c38bf58aab71f8880644b7cba04b1f377b6
Gastatouillle <bouchergaston4@gmail.com>
2026-03-10 17:35:33 +1100

    Tuned indexer speeds with aldrich

--- src/main/cpp/subsystems/SubIndexer.cpp
+++ src/main/cpp/subsystems/SubIndexer.cpp
@@ -53,7 +53,7 @@ void SubIndexer::SimulationPeriodic() {
 frc2::CommandPtr SubIndexer::Index() {
   return StartEnd(
     [this] {
-      _indexerMotor.Set(1);
+      _indexerMotor.Set(0.9);
     },
     [this] {
       _indexerMotor.Set(0);
