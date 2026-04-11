f5febc810846718e3d1f0f08b422f8707ad55423
Liam Stow <liam.stow@outlook.com>
2026-04-11 22:17:19 +1000

    added warning test code: unused variable

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -20,6 +20,9 @@
 #include <wpinet/WebServer.h>
 
 Robot::Robot() {
+  // remove me
+  int i = 3;
+
   // USB logging
   frc::DataLogManager::Start();
   frc::SmartDashboard::PutData(&frc2::CommandScheduler::GetInstance());
@@ -58,7 +61,6 @@ void Robot::DisabledExit() {}
 
 void Robot::AutonomousInit() {
   m_autonomousCommand = m_container.GetAutonomousCommand();
-
   if (m_autonomousCommand) {
     /* m_autonomousCommand is a shared_ptr<frc2::CommandPtr>. Compiler gets mad
      * at a single .get() but .Schedule has a override for a raw pointer to the
