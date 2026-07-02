188139cfdf9aacff2a51562245846d190364f820
s-haveesh <shaveesh@gmail.com>
2026-07-02 16:18:00 +1000

    Revert "Codex implementation of left-right flipping"
    This reverts commit 53d53ceb9092b5d4945731803ce66758348cddf7.

--- src/main/include/commands/AutonCommands.h
+++ src/main/include/commands/AutonCommands.h
@@ -1,13 +1,6 @@
 #include <frc2/command/Commands.h>
 
 namespace cmd {
-    enum class AutonSide {
-        RIGHT,
-        LEFT
-    };
-
-    void SetAutonSide(AutonSide side);
-
     frc2::CommandPtr TESTDriveInASquare();
     frc2::CommandPtr TESTForward250cm();
     frc2::CommandPtr TESTForward250cmWhileTurning();
